78e35457d4e072845ec14ef2d0d7d1c2f01230d9
Matěj Račinský <matej.racinsky@avast.com>
2020-04-03 13:11:32 +0200

    fixed tests, some minor fixes in codebase, some minor dependency keeping same version of Mill.

--- src/mask.jl
+++ src/mask.jl
@@ -7,22 +7,22 @@ struct Mask{I<:Union{Nothing, Vector{Int}}}
 	cluster_membership::I
 end
 
-holds the structure for explaining 
-mask --- a binary indicator vector, where each item corresponds to one explainable feature in the sample. 
-	It is used to prune or modulate the sample. 
+holds the structure for explaining
+mask --- a binary indicator vector, where each item corresponds to one explainable feature in the sample.
+	It is used to prune or modulate the sample.
 
-participate --- identifies if the corresponding item in the mask participate in the creation of modulated samples. 
-	For example imagine to have two bags, one nested in the other. If we remove instance(s) in the top bag, then this 
-	correnspods to whole bags in the bottom mil problem. Now whatever values the mask of these (removed bag) has, 
+participate --- identifies if the corresponding item in the mask participate in the creation of modulated samples.
+	For example imagine to have two bags, one nested in the other. If we remove instance(s) in the top bag, then this
+	correnspods to whole bags in the bottom mil problem. Now whatever values the mask of these (removed bag) has,
 	it does not have any effect on the sample, as they are effectively removed. For these samples we set participate
 	to zero, such that these items will not be counted in the statistics.
 
-outputid --- this is a vector which identifies to which sample the corresponding item belongs. This is used to 
+outputid --- this is a vector which identifies to which sample the corresponding item belongs. This is used to
 	speeedup the update of stats when multiple samples are used
 
 daf --- Shappley value statistics for each item (cluster of items)
 
-cluster_membership --- this identifies to which cluster the item belongs to. 
+cluster_membership --- this identifies to which cluster the item belongs to.
 	This is created if clustering of items is on
 """
 struct Mask{I<:Union{Nothing, Vector{Int}}}
@@ -44,7 +44,7 @@ Base.setindex!(m::Mask{Vector{Int}}, v, i::Int) = m.mask[m.cluster_membership .=
 ####
 #	Explaination without clustering, where each item is independent of others
 ####
-Mask(d::Int) = Mask(fill(true, d), fill(true, d), fill(0, d), Daf(d), nothing)
+Mask(d::Int) = Mask(fill(true, d), fill(true, d), fill(1, d), Daf(d), nothing)
 
 
 function StatsBase.sample!(m::Mask{Nothing})
@@ -61,12 +61,12 @@ end
 ####
 #	Explaination, where items are clustered together
 ####
-function Mask(cluster_membership::Vector{Int}) 
+function Mask(cluster_membership::Vector{Int})
 	cluster_membership = normalize_clusterids(cluster_membership)
 	n = length(unique(cluster_membership))
 	!isempty(setdiff(1:n, unique(cluster_membership))) && @show cluster_membership
 	d = length(cluster_membership)
-	Mask(fill(true, d), fill(true, d), fill(0, d), Daf(n), cluster_membership)
+	Mask(fill(true, d), fill(true, d), fill(1, d), Daf(n), cluster_membership)
 end
 
 _cluster_membership(ij::Vector{Int}, i) = ij[i]
@@ -87,5 +87,5 @@ function StatsBase.sample!(m::Mask{Vector{Int64}})
 	_mask = sample([true, false], maximum(ci))
 	for (i,k) in enumerate(ci)
 		m.mask[i] = _mask[k]
-	end 
+	end
 end
