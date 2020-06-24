c6f2a7c215b82231a7722a70d754a4248830b54b
Šimon Mandlík <simon.mandlik@avast.com>
2020-06-24 22:42:17 +0200

    Update hierarchical_utils.jl

--- src/hierarchical_utils.jl
+++ src/hierarchical_utils.jl
@@ -1,16 +1,14 @@
-import HierarchicalUtils: NodeType, childrenfields, children, InnerNode, SingletonNode, LeafNode, printtree, noderepr
+import HierarchicalUtils: NodeType, children, InnerNode, LeafNode, printtree, noderepr
 
 # for schema structures
 NodeType(::Type{T}) where T <: AbstractListMask = LeafNode()
 NodeType(::Type{T}) where T <: EmptyMask = LeafNode()
-NodeType(::Type{T}) where T <: BagMask = SingletonNode()
+NodeType(::Type{T}) where T <: BagMask = InnerNode()
 NodeType(::Type{T}) where T <: TreeMask = InnerNode()
 
 noderepr(n::AbstractExplainMask) = "$(Base.typename(typeof(n)))"
 noderepr(n::EmptyMask) = "skipped"
 string(Base.typename(BagMask))
-childrenfields(::Type{T}) where T <: BagMask = (:child,)
-childrenfields(::Type{TreeMask}) = (:childs,)
 
 children(n::BagMask) = (n.child,)
 children(n::TreeMask) = (; n.childs...)
