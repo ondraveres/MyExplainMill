41be5abbd0d212bb3b0ffb14881d51cb7af0c988
Šimon Mandlík <simon.mandlik@avast.com>
2020-08-17 08:49:57 +0200

    small fixes

--- src/matching.jl
+++ src/matching.jl
@@ -4,64 +4,64 @@ import Base.match
 # explanations (input to YaraGens) are correct
 
 function printontrue(o, verbose,  s...)
-	verbose && o && println(s)
-	o 
+    verbose && o && println(s)
+    o 
 end
 
-function Base.match(d::Dict, e, v; path = (), verbose = false)
-	isempty(d) && return(false)
-	ks = collect(keys(d))
-	if length(ks) == 1 
-		k = only(ks)
-		k == :or && return(match_or(d[:or], e, v; path = path, verbose = verbose))
-		k == :and && return(match_and(d[:and], e, v; path = path, verbose = verbose))
-	end
-	match_product(d, e, v; path = path, verbose = verbose)
+function Base.match(d::Dict, e, v; path=(), verbose=false)
+    isempty(d) && return(false)
+    ks = collect(keys(d))
+    if length(ks) == 1 
+        k = only(ks)
+        k == :or && return(match_or(d[:or], e, v; path=path, verbose=verbose))
+        k == :and && return(match_and(d[:and], e, v; path=path, verbose=verbose))
+    end
+    match_product(d, e, v; path = path, verbose = verbose)
 end
 
 function Base.match(s::Vector, e, v::ProductNode; path = (), verbose = false)
-	all(map(x -> match(x, e, v, path = path, verbose = verbose), s))
+    all(map(x -> match(x, e, v, path = path, verbose = verbose), s))
 end
 
 
 function match_product(d::Dict, e, v::ProductNode; path = (), verbose = false)
-	ks = intersect(keys(d), keys(v))
-	all(map(k -> match(d[k], e[k], v[k]; path = (path..., k), verbose = verbose), collect(ks)))
+    ks = intersect(keys(d), keys(v))
+    all(map(k -> match(d[k], e[k], v[k]; path = (path..., k), verbose = verbose), collect(ks)))
 end
 
 function Base.match(s, e, v::BagNode; path = (), verbose = false)
-	match(s, e.item, v.data; path = (path..., :item), verbose = verbose)
+    match(s, e.item, v.data; path = (path..., :item), verbose = verbose)
 end
 
 function Base.match(d::Dict, e, v::BagNode; path = (), verbose = false)
-	ks = collect(keys(d))
-	if length(ks) == 1 
-		k = only(ks)
-		k == :or && return(match_or(d[:or], e, v; path = path, verbose = verbose))
-		k == :and && return(match_and(d[:and], e, v; path = path, verbose = verbose))
-	end
-	error("matching Dict to BagNode does not make much sense")
+    ks = collect(keys(d))
+    if length(ks) == 1 
+        k = only(ks)
+        k == :or && return(match_or(d[:or], e, v; path = path, verbose = verbose))
+        k == :and && return(match_and(d[:and], e, v; path = path, verbose = verbose))
+    end
+    error("matching Dict to BagNode does not make much sense")
 end
 
 function Base.match(s::Vector, e, v::LazyNode{T,Array{String,1}}; path = (), verbose = false) where {T}
-	all(map(x -> match(x, e, v, path = path, verbose = verbose), s))
+    all(map(x -> match(x, e, v, path = path, verbose = verbose), s))
 end
 
 function Base.match(s::String, e, v::ArrayNode{T,N}; path = (), verbose = false) where {T<: NGramMatrix, N}
-	printontrue(s ∈ v.data.s, verbose, path," ", s)
+    printontrue(s ∈ v.data.s, verbose, path," ", s)
 end
 
 function Base.match(s::Vector, e, v::ArrayNode{T,N}; path = (), verbose = false) where {T<: NGramMatrix, N}
-	all(x -> printontrue(x ∈ v.data.s, verbose, path," ", x), s)
+    all(x -> printontrue(x ∈ v.data.s, verbose, path," ", x), s)
 end
 
 function Base.match(s::String, e::ExtractCategorical, v::ArrayNode{T,N}; path = (), verbose = false) where {T<:Flux.OneHotMatrix, N}
-	idxs = map(i -> i.ix, v.data.data)
-	printontrue(e.keyvalemap[s] ∈ idxs, verbose, path," ", s)
+    idxs = map(i -> i.ix, v.data.data)
+    printontrue(e.keyvalemap[s] ∈ idxs, verbose, path," ", s)
 end
 
 function Base.match(s::Vector, e::ExtractCategorical, v::ArrayNode{T,N}; path = (), verbose = false) where {T<:Flux.OneHotMatrix, N}
-	all(map(x -> match(x, e, v, path = path, verbose = verbose), s))
+    all(map(x -> match(x, e, v, path = path, verbose = verbose), s))
 end
 
 match_and(ds::Vector, e, v; path = (), verbose = false) = all(match(d, e, v; path = path, verbose = verbose) for d in  ds)
