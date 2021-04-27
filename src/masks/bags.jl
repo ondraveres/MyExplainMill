55917b15f36eb5a1c0d618a9610fc8e716a137d5
Tomáš Pevný <tomas.pevny@avast.com>
2021-04-27 09:20:54 +0200

    fixed a bug in BagMask creation

--- src/masks/bags.jl
+++ src/masks/bags.jl
@@ -10,7 +10,7 @@ function Mask(ds::BagNode, model::BagModel, initstats, cluster; verbose::Bool = 
 	isnothing(ds.data) && return(EmptyMask())
 	nobs(ds.data) == 0 && return(EmptyMask())
 	child_mask = Mask(ds.data, model.im, initstats, cluster)
-	cluster_assignments = cluster(model.im, ds.data)
+	cluster_assignments = cluster(model, ds)
 	if verbose
 		n, m = nobs(ds.data), length(unique(cluster_assignments))
 		println("number of instances: ", n, " ratio: ", round(m/n, digits = 3))
