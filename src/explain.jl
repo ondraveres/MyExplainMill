49ad52ff75eeab1f52759bf826c791a9e74e63d3
Tomáš Pevný <tomas.pevny@avast.com>
2021-12-03 15:40:22 +0100

    fixed remove metadata

--- src/explain.jl
+++ src/explain.jl
@@ -49,7 +49,8 @@ function explain(e, ds::AbstractNode, model::AbstractMillModel, extractor::JsonG
     mk = explain(e, dssm, model; kwargs...)
     e2boolean(ds, mk, extractor)
 end
-_remove_metadata(ds::Mill.ProductNode) = Mill.ProductNode(_remove_metadata(ds.data), nothing)
+
+_remove_metadata(ds::Mill.ProductNode) = Mill.ProductNode(map(_remove_metadata, ds.data), nothing)
 _remove_metadata(ds::Mill.ArrayNode) = Mill.ArrayNode(ds.data, nothing)
 _remove_metadata(ds::Mill.BagNode) = Mill.BagNode(ds.data, ds.bags, nothing)
 
