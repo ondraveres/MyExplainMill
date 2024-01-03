c887459874e4d2aa90d60b0247cd6a98d950357a
Šimon Mandlík <simon.mandlik@gmail.com>
2024-01-03 13:07:00 +0100

    fixed docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,13 +3,15 @@ using Documenter, DocumenterCitations
 using ExplainMill
 
 makedocs(
-         CitationBibliography(joinpath(@__DIR__, "references.bib"), style=:numeric),
          sitename = "ExplainMill.jl",
          format = Documenter.HTML(sidebar_sitename=false,
                                   collapselevel=2,
                                   assets=["assets/favicon.ico", "assets/custom.css"]),
-         strict = [:eval_block, :example_block, :meta_block, :setup_block],
+         warnonly = Documenter.except(:eval_block, :example_block, :meta_block, :setup_block),
          modules = [ExplainMill],
+         plugins = [
+             CitationBibliography(joinpath(@__DIR__, "references.bib"), style=:numeric)
+         ],
          pages = [
                   "Home" => "index.md",
                   "Manual" => [],
